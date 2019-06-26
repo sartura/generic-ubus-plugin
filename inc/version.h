c78fd29e5afc62962782c08fdfe9928b406aa0af
Luka Paulic <luka.paulic0@gmail.com>
2019-06-26 07:03:36 +0200

    Updated and tested end-to-end.toml

--- inc/version.h
+++ inc/version.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-const char *GIT_SHA1 = "GIT_SHA1_ea19e57a187102055b6b217fff3b740a0599452e";
+const char *GIT_SHA1 = "GIT_SHA1_9b0f8479c49e3f046cef7d1406602e7a6951c6f5";
 const char *COMPILED_SYSREPO_VERSION = "COMPILED_SYSREPO_VERSION_0.7.7";
 
 // check if Sysrepo < 0.7.5 is used
